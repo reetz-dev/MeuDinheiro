8383881111b1a36513b36424eeb0c51f3647e004
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2025-09-26 04:49:52 -0300

    Initial commit

---
+++ src/main/java/com/reetz/MeuDinheiro/MeuDinheiroApplication.java
@@ -0,0 +1,13 @@
+package com.reetz.MeuDinheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuDinheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuDinheiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reetz/MeuDinheiro/MeuDinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reetz.MeuDinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuDinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
